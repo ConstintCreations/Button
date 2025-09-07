88b5df009918af3777994f163b38a8250d5fd9c0
ConstintCreations <ethanglenn1208@gmail.com>
2025-09-07 14:08:32 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const outerButton = document.querySelector('.outer-button');
+const innerButton = document.querySelector('.inner-button');
+const buttonOuterBase = document.querySelector('.button-outer-base');
+const buttonInnerBase = document.querySelector('.button-inner-base');
